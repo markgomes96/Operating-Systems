06e9146d0967722ad354042936fdf13af4fc74d5
Mark Gomes <markgomes96@gmail.com>
2019-01-21 22:36:44 -0500

    Finished PitchFork

--- fork1/forky.c
+++ fork1/forky.c
@@ -11,10 +11,10 @@ int main(int argc, char** argv)
 {
 	int num = 0;
 
-	if(argc > 1)
+	if(argc > 1)	    //check if an argument was entered
 	{
 		num = atoi(argv[1]);
-		if(num < 0 || num > 5)
+		if(num < 0 || num > 5)	//check if number is within range
 		{
 			printf("Number out of range : 0 <= N <= 5 \n");
 			exit(0);
@@ -25,4 +25,29 @@ int main(int argc, char** argv)
 		printf("No number argument found \n");
 		exit(0);
 	}
+
+	int a = 0;
+	int x,y;
+	printf("Gen \t PID \t PPID \n");		//set up header
+	
+	for(x = 1; x <= num; x++) 	//iterates throught the generations
+	{
+		for(y = 0; y < x; y++)		//interate through children for each generation
+		{
+			a = fork();		//spawn a child form parent
+			if(a == 0) break;	//remove child form child spawning
+		}
+		if(a != 0)		//remove parents in current generation
+		{
+			printf("%i \t %d \t %d \n", (x-1), getpid(), getppid());
+			break;
+		}
+	}
+
+	if(a == 0)	//prints out info for children of current generation
+		printf("%i \t %d \t %d \n", num, getpid(), getppid());
+	
+	sleep(1);
+
+	return 0;
 }
